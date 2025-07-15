670d8de6ff6612bc2991f9bd954b451fe1d2238d
diana solovey <diansolo40@gmail.com>
2025-07-15 17:56:20 +0300

    init: load tools and initialyze work with testing components

---
+++ src/components/ErrorButton.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import { ErrorButton } from './errorButton';
+import React from 'react';
+import '@testing-library/jest-dom';
+
+test('renders ErrrorButton component', () => {
+  render(<ErrorButton />);
+  const linkElement = screen.getByText(/Check error boundary/i);
+  expect(linkElement).toBeInTheDocument();
+});
