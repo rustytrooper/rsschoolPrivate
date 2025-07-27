ad7a8f75f281d54f8ed33894f94d951be2320b5a
diana solovey <diansolo40@gmail.com>
2025-07-28 02:45:42 +0300

    fix: fix about test

--- src/components/About/About.test.tsx
+++ src/components/About/About.test.tsx
@@ -7,11 +7,6 @@ describe('About', () => {
   it('renders the correct content', () => {
     render(<About />);
 
-    const heading = screen.getByText(
-      /What about me\? Do you really wanna hurt me, baby\?/i
-    );
-    expect(heading).toBeInTheDocument();
-
     const additionalText = screen.getByText(
       /Do you wanna break my heart to pieces\?/i
     );
